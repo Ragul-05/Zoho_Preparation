61a4cfb1e0ef6928f09231049afd23bea1b6f699
Ragul-05 <ragulgopinath2005@gmail.com>
2025-06-18 18:09:52 +0530

    Added the file for finding unique elements in the array

---
+++ SingleNonDulicate.java
@@ -0,0 +1,23 @@
+public class SingleNonDulicate {
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+        System.out.println(singleNonDuplicate(nums));
+    }
+
+    static int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
+
+}
