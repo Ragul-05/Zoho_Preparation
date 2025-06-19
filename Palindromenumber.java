1df8246ec516990f215f9a7331804cc5e73cbcbb
Ragul-05 <ragulgopinath2005@gmail.com>
2025-06-19 20:48:04 +0530

    Adding the Palindromenumber.java file

---
+++ Palindromenumber.java
@@ -0,0 +1,14 @@
+class PalindromeNumber {
+    public boolean isPalindrome(int n) {
+        int original = n;
+        int reversed = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+
+        return original == reversed;
+    }
+}
